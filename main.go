52ceb738b1c648e13216fa82324673345433de9a
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-02-10 21:37:46 -0300

    feat: add bootstrap 5

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -13,27 +12,41 @@ type Film struct {
 	Director string
 }
 
+type Actor struct {
+	Name    string
+	Birth   string
+	Country string
+}
+
+type Data struct {
+	Films  []Film
+	Series []Film
+	Actors []Actor
+}
+
 func main() {
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
-		films := []Film{
-			{Title: "The Shawshank Redemption", Year: 1994, Director: "Frank Darabont"},
-			{Title: "The Godfather", Year: 1972, Director: "Francis Ford Coppola"},
-			{Title: "The Dark Knight", Year: 2008, Director: "Christopher Nolan"},
-			{Title: "Blade Ruuner", Year: 1982, Director: "Ridley Scott"},
-		}
-		data := map[string][]Film{
-			"Films": films,
-			"Series": {
+		data := Data{
+			Films: []Film{
+				{Title: "The Shawshank Redemption", Year: 1994, Director: "Frank Darabont"},
+				{Title: "The Godfather", Year: 1972, Director: "Francis Ford Coppola"},
+				{Title: "The Dark Knight", Year: 2008, Director: "Christopher Nolan"},
+				{Title: "Blade Runner", Year: 1982, Director: "Ridley Scott"},
+			},
+			Series: []Film{
 				{Title: "Breaking Bad", Year: 2008, Director: "Vince Gilligan"},
 				{Title: "Game of Thrones", Year: 2011, Director: "David Benioff"},
 				{Title: "The Crown", Year: 2016, Director: "Peter Morgan"},
 			},
+			Actors: []Actor{
+				{Name: "Tom Hanks", Birth: "9 July 1956", Country: "USA"},
+				{Name: "Morgan Freeman", Birth: "1 June 1937", Country: "USA"},
+				{Name: "Al Pacino", Birth: "25 April 1940", Country: "USA"},
+				{Name: "Christian Bale", Birth: "30 January 1974", Country: "UK"},
+			},
 		}
-		// log each film
-		for _, film := range films {
-			fmt.Println(film.Title, film.Year, film.Director)
-		}
+
 		tmpl.Execute(w, data)
 	}
 
